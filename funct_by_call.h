eceff7f4fe368247df1f48dcc1090c2d856582ac
Uziel Gaeta <ugaeta@cpp.edu>
2021-10-12 01:08:57 -0700

    Still some errors but getting around to completing functions and implementations

--- funct_by_call.h
+++ funct_by_call.h
@@ -7,3 +7,39 @@
 #include "address_book_menu.h"
 #include "address_book.h"
 
+static ContactInfo* searchAddressBook(ContactInfo *ptr, int bookSize, const void *targetPtr,
+                            int (*functionPtr)(const void *, ContactInfo*, int)){
+    
+    int counter = 0;
+    printf("SearchAddressBook\n");
+    ContactInfo *endPtr = ptr + bookSize;
+    for(; ptr < endPtr; ptr++)
+        printf("ptr: %s\n",ptr);
+
+        if(counter < (sizeof(ptr->name)/sizeof(ptr->name[0])))
+            counter ++;
+
+        if((*functionPtr)(targetPtr, ptr, counter) == 0){
+            return (ContactInfo*) ptr;
+        }
+    return NULL;
+
+}
+
+static int compareName(const void *targetPtr, ContactInfo *tableValuePtr, int index){
+    printf("CompareName\n");
+    return strcmp((char *) targetPtr, (char *) tableValuePtr -> name[index]);
+}
+
+static int comparePhNum(const void *targetPtr, ContactInfo *tableValuePtr, int index){
+    return strcmp((char *) targetPtr, (char *) tableValuePtr -> phone_numbers[index]);
+}
+
+ContactInfo* searchByName(ContactInfo *ptr, int size, char *name){
+    printf("\nLooking for %s\n", name);
+    return searchAddressBook(ptr, size, name, compareName);
+}
+
+ContactInfo* searchByPhNum(ContactInfo *ptr, int size, char *phoneNum){
+    return searchAddressBook(ptr, size, phoneNum, comparePhNum);
+}
