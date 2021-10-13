112af4006d7ecb1a098bd3914df976dfd40094ab
Uziel Gaeta <ugaeta@cpp.edu>
2021-10-12 23:56:06 -0700

    My testing file

--- test.c
+++ test.c
@@ -18,7 +18,10 @@ int main(int argc, char const *argv[])
     char name[32];
 
     strcpy(addBook.list[0].name[0], "Uzi");
-
+    strcpy(addBook.list[0].phone_numbers[0], "3235561034");
+    strcpy(addBook.list[0].phone_numbers[1], "3234480109");
+    addBook.list[0].si_no = 323;
+    printf("phone num is: %s", addBook.list[0].phone_numbers[0]);
     printf("%d", sizeof(addBook.list[0].name[0]));
 
     Status delete_contact(AddressBook *address_book);
